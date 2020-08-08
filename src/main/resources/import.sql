9b30813cfd976669802d739c830e561a53dbc1c8
Dan Bondarenko <danxfer@gmail.com>
2020-08-08 19:12:53 +0300

    Configure database connection details and test data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO filter (id, enabled, evaluation_strategy, name) VALUES (0, FALSE, 'ALL_MATCH', 'default');
+INSERT INTO predicate (id) VALUES (0);
+INSERT INTO numerical_predicate (condition, fixed_operand, id) VALUES ('EQUAL', 0, 0);
+INSERT INTO filter_predicates (filter_id, predicates_id) VALUES (0, 0);
