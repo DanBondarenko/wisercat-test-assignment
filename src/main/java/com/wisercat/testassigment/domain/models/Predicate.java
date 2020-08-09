6b065dd717845a3783e309d024336883ada712d2
Dan Bondarenko <danxfer@gmail.com>
2020-08-09 17:47:08 +0300

    Configure Jackson subtype conversion for Predicate objects

--- src/main/java/com/wisercat/testassigment/domain/models/Predicate.java
+++ src/main/java/com/wisercat/testassigment/domain/models/Predicate.java
@@ -1,13 +1,26 @@
 package com.wisercat.testassigment.domain.models;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.wisercat.testassigment.domain.models.evaluation.Evaluatable;
-import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
-@NoArgsConstructor
+@Data
 @Inheritance(strategy = InheritanceType.JOINED)
+@JsonTypeInfo(use = Id.NAME, property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = DatedPredicate.class),
+    @JsonSubTypes.Type(value = TextualPredicate.class),
+    @JsonSubTypes.Type(value = NumericalPredicate.class)
+})
 public abstract class Predicate extends BaseEntity {
-    abstract boolean evaluate(Evaluatable evaluatableObject);
+    abstract boolean evaluate(Evaluatable evaluableObject);
 }
